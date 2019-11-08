38ec99e762522d1ab5c5acb6c48dbe60eddc33c1
olusola olagoroye <solaolagoroye@gmail.com>
2019-11-08 18:48:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var counter = [];
+  for (let i = 0; i < musicians.length; i++){
+  counter.push(`${musicians[i]} plays ${instruments[i]}`);
+}
+  return counter;
+}
+function johnLennonFacts(facts){
+ let i = 0 ;
+while (i < facts.length){
+ facts[i] = `${facts[i]}!!!`;
+ i++;
+ console.log(facts[i]);
+}
+ return facts;
+}
+
+function iLoveTheBeatles(n){
+  let i = [];
+  do { iLoveTheBeatles[i] = `${iLoveTheBeatles[i]} I love the Beatles!`;
+  i++;
+    console.log(iLoveTheBeatles[i]);
+  } while (i < 17);
+
+
+return iLoveTheBeatles;
+}
